ca38fdbb49740400feefc0969d9036f95790adf0
skektec <skekung.yt@gmail.com>
2024-12-02 12:46:18 +0200

    Fixed userID nickname replacement

--- nabber.js
+++ nabber.js
@@ -1,31 +1,5 @@
 const fs = require('fs');
 
-let replacements = [
-    { target: '473024549370462209', replacement: 'Kspace' },
-    { target: '551478150627065862', replacement: 'Cyne' },
-    { target: '703303649870217309', replacement: 'Skeksis' },
-    { target: '535118345004122142', replacement: 'Mia' },
-    { target: '313366936341708819', replacement: 'P3xA' },
-    { target: '690954746344767538', replacement: '017' },
-    { target: '919635711026692107', replacement: 'LizardBlizzard' },
-    { target: '803369229360431134', replacement: 'Blu' },
-    { target: '609100564235681803', replacement: 'Headip Shitpin' },
-    { target: '486144133858197515', replacement: 'Fizzgig' },
-    { target: '795939963220525087', replacement: 'tthhuunnddrr' },
-    { target: '289028991069978624', replacement: 'Memes' },
-    { target: '699676778095968407', replacement: 'Sophie' },
-    { target: '673227352880316416', replacement: 'Destroyer' },
-    { target: '698160735376769084', replacement: 'Jobear' },
-    { target: '702803260175679490', replacement: 'chlorinechloe' },
-    { target: '332576819125420033', replacement: 'Glu' },
-    { target: '430457743577579530', replacement: 'ebinmemes' },
-    { target: '498401858520612865', replacement: 'shibusu' },
-    { target: '590414231409786880', replacement: 'someweirdperson' },
-    { target: '413409129110175754', replacement: 'karno' },
-    { target: '85614143951892480', replacement: 'cocks :yum:' }
-];
-
-
 // This counts the amount of qoutes from each person
 fs.readFile('./qoutes.json', 'utf8', (err, data) => {
     if (err) {
@@ -41,7 +15,7 @@ fs.readFile('./qoutes.json', 'utf8', (err, data) => {
         counts[name] = (counts[name] || 0) + 1;
     });
 
-    const updatedCounts = nameFromId(counts, replacements);
+    const updatedCounts = nameFromId(counts);
 
     const sortedCounts = sortCountsByQuotes(updatedCounts);
 
@@ -49,11 +23,11 @@ fs.readFile('./qoutes.json', 'utf8', (err, data) => {
 });
 
 //Replaces the userId with a nickname
-function nameFromId(score, replacements) {
+function nameFromId(score) {
     const updatedScore = {};
     for (const userId in score) {
-        const replacement = replacements.find((pair) => pair.target === userId)?.replacement || userId;
-        updatedScore[replacement] = score[userId];
+        const updatedCounts = `<@${userId}>`;
+        updatedScore[updatedCounts] = score[userId];
     }
     return updatedScore;
 }
