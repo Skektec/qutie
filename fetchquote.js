da0257be65b78fe92aa4dc1436d9ddc5e364e433
Skektec <Skekung.yt@gmail.com>
2025-01-04 20:13:00 +0200

    Enabled fetching quotes via username

--- fetchquote.js
+++ fetchquote.js
@@ -4,6 +4,7 @@ const { EmbedBuilder } = require('discord.js')
 module.exports = {
     execute: async (message, args) => {
         const quoteId = args[0]
+        const nick = args[0]
 
         try {
             const data = await fs.promises.readFile('./quotes.json', 'utf8')
@@ -14,14 +15,51 @@ module.exports = {
                 return
             }
 
-            if ((args[0] && args[0].startsWith('<@')) || quoteId) {
-                const userId = args[0].replace(/[<@!>]/g, '')
-                const userQuotes = quotes.filter((q) => q.userId === userId)
+            if ((args[0] && args[0].startsWith('<@')) || quoteId || nick) {
+                let userId
+                const nick = args[0]
 
-                if (userQuotes.length > 0) {
+                if (args[0].startsWith('<@')) {
+                    userId = args[0].replace(/[<@!>]/g, '')
+                }
+
+                const userQuotesId = quotes.filter((q) => q.userId === userId)
+                const userQuotesNick = quotes.filter((q) => q.nick === nick)
+
+                if (userQuotesNick.length > 0) {
                     const randomQuote =
-                        userQuotes[
-                            Math.floor(Math.random() * userQuotes.length)
+                        userQuotesNick[
+                            Math.floor(Math.random() * userQuotesNick.length)
+                        ]
+
+                    const quoteTimestamp = randomQuote.time * 1000
+                    const quoteDate = new Date(quoteTimestamp)
+                    const formattedDate = quoteDate.toLocaleString('en-US', {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: 'numeric',
+                        minute: 'numeric',
+                        second: 'numeric',
+                        hour12: true,
+                    })
+
+                    const quoteEmbed = new EmbedBuilder()
+                        .setColor(0x0099ff)
+                        .setTitle(`#${randomQuote.id}`)
+                        .setDescription(
+                            `${randomQuote.text} \n - <@${randomQuote.userId}> [(Jump)](https://discordapp.com/channels/${randomQuote.server}/${randomQuote.channel}/${randomQuote.messageId})`
+                        )
+                        .setFooter({
+                            text: `${formattedDate}`,
+                        })
+
+                    message.channel.send({ embeds: [quoteEmbed] })
+                } else if (userQuotesId.length > 0) {
+                    const randomQuote =
+                        userQuotesId[
+                            Math.floor(Math.random() * userQuotesId.length)
                         ]
 
                     const quoteTimestamp = randomQuote.time * 1000
