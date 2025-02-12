d55396a9bf85ddba31f72ad7c172c0cd05c7c8c0
Skekung <skekung.yt@gmail.com>
2025-02-12 09:35:47 +0200

    Added mistral ai

--- jarvis_commands/gif.js
+++ jarvis_commands/gif.js
@@ -1,38 +1,32 @@
-const { tenorToken } = require('../data/config.json')
+const { tenorToken } = require('../data/config.json');
 
 module.exports = {
-    execute: async (message) => {
-        const commandSen = message.content.split(' ')
+	execute: async (match) => {
+		const message = match;
 
-        const ofIndex = commandSen.indexOf('of')
+		const searchTerm = match[1];
 
-        if (ofIndex === -1 || ofIndex === commandSen.length - 1) {
-            message.reply('What am I searching?')
-            return
-        }
+		try {
+			const fetch = (await import('node-fetch')).default;
 
-        const searchTerm = commandSen.slice(ofIndex + 1).join(' ')
+			const response = await fetch(
+				`https://tenor.googleapis.com/v2/search?q=${encodeURIComponent(
+					searchTerm
+				)}&key=${tenorToken}&limit=1`
+			);
+			console.log(response + '1');
 
-        try {
-            const fetch = (await import('node-fetch')).default
+			const data = await response.json();
 
-            const response = await fetch(
-                `https://tenor.googleapis.com/v2/search?q=${encodeURIComponent(
-                    searchTerm
-                )}&key=${tenorToken}&limit=1`
-            )
-
-            const data = await response.json()
-
-            if (data.results && data.results.length > 0) {
-                const gifUrl = data.results[0].url
-                message.reply(gifUrl)
-            } else {
-                message.reply('No GIFs found for your search term.')
-            }
-        } catch (error) {
-            console.error('Error fetching GIF:', error)
-            message.reply('An error occurred while fetching the GIF.')
-        }
-    },
-}
+			if (data.results && data.results.length > 0) {
+				const gifUrl = data.results[0].url;
+				// message.reply(gifUrl + ' 7');
+			} else {
+				message.reply('No GIFs found for your search term.');
+			}
+		} catch (error) {
+			console.error('Error fetching GIF:', error);
+			message.reply('An error occurred while fetching the GIF.');
+		}
+	},
+};
