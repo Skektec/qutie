69d217b7e48a172549bcac510a1b7ca742c2bd55
Skekung <skekung.yt@gmail.com>
2025-02-15 18:33:36 +0200

    Quoting will show who the message is from

--- events/addquote.js
+++ events/addquote.js
@@ -1,28 +1,27 @@
-const sqlite3 = require("sqlite3").verbose();
-const database = new sqlite3.Database("./data/general.db");
+const sqlite3 = require('sqlite3').verbose();
+const database = new sqlite3.Database('./data/general.db');
 
 module.exports = {
-  execute: async (reaction, bot, user) => {
-    const ogBastard = user;
-    const botHasReacted = await reaction.users
-      .fetch()
-      .then((users) => users.has(bot.user.id));
+	execute: async (reaction, bot, user) => {
+		const botHasReacted = await reaction.users
+			.fetch()
+			.then((users) => users.has(bot.user.id));
 
-    if (botHasReacted) {
-      return;
-    }
+		if (botHasReacted) {
+			return;
+		}
 
-    const nick = reaction.message.author.username;
-    const userId = reaction.message.author.id;
-    const channel = reaction.message.channel.id;
-    const server = reaction.message.guildId;
-    const text = reaction.message.content;
-    const messageId = reaction.message.id;
-    const time = Math.floor(reaction.message.createdTimestamp / 1000);
-    const tableName = `${server}-quotes`;
+		const nick = reaction.message.author.username;
+		const userId = reaction.message.author.id;
+		const channel = reaction.message.channel.id;
+		const server = reaction.message.guildId;
+		const text = reaction.message.content;
+		const messageId = reaction.message.id;
+		const time = Math.floor(reaction.message.createdTimestamp / 1000);
+		const tableName = `${server}-quotes`;
 
-    database.run(
-      `CREATE TABLE IF NOT EXISTS "${tableName}" (
+		database.run(
+			`CREATE TABLE IF NOT EXISTS "${tableName}" (
         nick TEXT,
         userId TEXT,
         channel TEXT,
@@ -31,44 +30,44 @@ module.exports = {
         messageId TEXT UNIQUE,
         time INTEGER
       )`,
-      (err) => {
-        if (err) console.error(`Error creating table ${tableName}:`, err);
-      }
-    );
+			(err) => {
+				if (err) console.error(`Error creating table ${tableName}:`, err);
+			}
+		);
 
-    database.get(
-      `SELECT 1 FROM "${tableName}" WHERE messageId = ? LIMIT 1`,
-      [messageId],
-      (err, row) => {
-        if (err) {
-          console.error(`Error checking for duplicate in ${tableName}:`, err);
-          return;
-        }
+		database.get(
+			`SELECT 1 FROM "${tableName}" WHERE messageId = ? LIMIT 1`,
+			[messageId],
+			(err, row) => {
+				if (err) {
+					console.error(`Error checking for duplicate in ${tableName}:`, err);
+					return;
+				}
 
-        if (row) {
-          return;
-        }
+				if (row) {
+					return;
+				}
 
-        const insertQuery = `
+				const insertQuery = `
           INSERT INTO "${tableName}" (nick, userId, channel, server, text, messageId, time)
           VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
-        database.run(
-          insertQuery,
-          [nick, userId, channel, server, text, messageId, time],
-          function (err) {
-            if (err) {
-              console.error(`Error inserting into ${tableName}:`, err);
-            } else {
-              reaction.message.react("💬");
-              reaction.message.reply({
-                content: `New quote added by ${ogBastard.username} as #${this.lastID}\n"${text}"`,
-                allowedMentions: { repliedUser: false },
-              });
-            }
-          }
-        );
-      }
-    );
-  },
+				database.run(
+					insertQuery,
+					[nick, userId, channel, server, text, messageId, time],
+					function (err) {
+						if (err) {
+							console.error(`Error inserting into ${tableName}:`, err);
+						} else {
+							reaction.message.react('💬');
+							reaction.message.reply({
+								content: `New quote added by ${user.username} as #${this.lastID}\n"${text}" - ${nick}`,
+								allowedMentions: { repliedUser: false },
+							});
+						}
+					}
+				);
+			}
+		);
+	},
 };
