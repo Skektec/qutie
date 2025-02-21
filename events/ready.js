6961d22aafd26af694d921bff86f25a010848440
Skekung <skekung.yt@gmail.com>
2025-02-22 01:32:34 +0200

    Added a notification for myself to see how many servers to bots in on boot

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,19 @@
-const { Events } = require("discord.js");
+const { Events } = require('discord.js');
+const { serverChannel } = require('../data/config.json');
 
 module.exports = {
-  name: Events.ClientReady,
-  once: true,
-  execute(client) {
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-    client.user.setActivity("with your mom", { type: 1 });
-  },
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity('with your mom', { type: 1 });
+
+		async () => {
+			await client.guilds.cache.fetch();
+			let serverCount = client.guilds.cache.size;
+			serverChannel.send(
+				`<@703303649870217309>! I am online in ${serverCount} servers!`
+			);
+		};
+	},
 };
