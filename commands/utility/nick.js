d6481ebde55a67d7cfcdb57fd9d48343c326356d
Skek <skekung.yt@gmail.com>
2025-02-24 14:24:28 +0200

    Addeed nickname changing command

---
+++ commands/utility/nick.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('nick')
+		.setDescription('Change a users nickname')
+		.addStringOption((option) =>
+			option
+				.setName('user')
+				.setDescription(`Who's nickname to change.`)
+				.setRequired(true)
+		)
+		.addStringOption((option) =>
+			option
+				.setName('nick')
+				.setDescription('What you want to change their nickname to.')
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		const user = interaction.options.getString('user');
+		const nick = interaction.options.getString('nick');
+		message.guild.members.get(user.id).setNickname(nick);
+	},
+};
