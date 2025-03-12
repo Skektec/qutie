a56c2b66a4c330e1a5f08ac12cc94b40b0d7359a
Skek <skekung.yt@gmail.com>
2025-03-12 21:12:04 +0200

    Deleted Quotes saved just incase

--- commands/quotes/deleteQuote.js
+++ commands/quotes/deleteQuote.js
@@ -4,6 +4,11 @@ const path = require('path');
 const Database = require('better-sqlite3');
 const dbPath = path.resolve(__dirname, '../../data/general.db');
 const database = new Database(dbPath);
+const fs = require('fs');
+const deletedQuotesPath = path.resolve(
+	__dirname,
+	'../../data/deletedQuotes.json'
+);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -19,7 +24,6 @@ module.exports = {
 
 	async autocomplete(interaction) {
 		const focusedOption = interaction.options.getFocused(true);
-		let choices = [];
 
 		if (focusedOption.name === 'id') {
 			try {
@@ -30,7 +34,7 @@ module.exports = {
 					.get();
 
 				if (!row) {
-					return await interaction.respond([]);
+					return await interaction.respond('No row');
 				}
 
 				await interaction.respond([
@@ -44,16 +48,35 @@ module.exports = {
 
 	async execute(interaction) {
 		const id = interaction.options.getString('id');
+
 		try {
-			const stmt = database.prepare(
+			const selectStmt = database.prepare(
+				`SELECT * FROM "${interaction.guild.id}-quotes" WHERE rowid = ?`
+			);
+			const quote = selectStmt.get(id);
+
+			if (!quote) {
+				return await interaction.reply(`No quote found with ID: #${id}`);
+			}
+
+			let deletedQuotes = [];
+			if (fs.existsSync(deletedQuotesPath)) {
+				deletedQuotes = JSON.parse(fs.readFileSync(deletedQuotesPath, 'utf8'));
+			}
+
+			deletedQuotes.push({ id, guildId: interaction.guild.id, ...quote });
+
+			fs.writeFileSync(
+				deletedQuotesPath,
+				JSON.stringify(deletedQuotes, null, 2)
+			);
+
+			const deleteStmt = database.prepare(
 				`DELETE FROM "${interaction.guild.id}-quotes" WHERE rowid = ?`
 			);
-			const result = stmt.run(id);
-			if (result.changes > 0) {
-				await interaction.reply(`Quote deleted: #${id}`);
-			} else {
-				await interaction.reply(`No quote found with ID: #${id}`);
-			}
+			deleteStmt.run(id);
+
+			await interaction.reply(`Deleted quote: #${id} - "${quote.text}"`);
 		} catch (err) {
 			errorLog.execute('Delete Func Error: ' + err.message);
 			await interaction.reply({
