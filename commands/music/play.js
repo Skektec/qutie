ceb3e55ba685d6d3ae6a2d24fb21bb7caa7a86db
Skek <skekung.yt@gmail.com>
2025-04-04 16:50:20 +0200

    More potential fixes

--- commands/music/play.js
+++ commands/music/play.js
@@ -8,9 +8,6 @@ const { Player, QueryType, useMainPlayer } = require('discord-player');
 const { DefaultExtractors } = require('@discord-player/extractor');
 const { getClient } = require('../../data/clientInstance');
 
-const client = getClient();
-const player = new Player(client);
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
@@ -23,60 +20,87 @@ module.exports = {
 		),
 
 	async autocompleteRun(interaction) {
-		const player = useMainPlayer();
-		await player.extractors.loadMulti(DefaultExtractors);
+		try {
+			const player = useMainPlayer();
+			await player.extractors.loadMulti(DefaultExtractors);
 
-		const query = interaction.options.getString('song', true);
-		if (!query) {
+			const query = interaction.options.getString('song', true);
+			if (!query) {
+				return interaction.respond([]);
+			}
+
+			const results = await player.search(query, {
+				searchEngine: QueryType.AUTO
+			});
+
+			return interaction.respond(
+				results.tracks.slice(0, 10).map((t) => ({
+					name: t.title || 'Unknown Title',
+					value: t.url || '',
+				}))
+			);
+		} catch (error) {
+			console.error('Autocomplete error:', error);
 			return interaction.respond([]);
 		}
-
-		const results = await player.search(query);
-
-		console.log(results);
-
-		return interaction.respond(
-			results.tracks.slice(0, 10).map((t) => ({
-				name: t.title || 'Unknown Title',
-				value: t.url || '',
-			}))
-		);
 	},
 
 	async execute(interaction) {
-		const player = useMainPlayer();
-		await player.extractors.loadMulti(DefaultExtractors);
+		try {
+			const player = useMainPlayer();
+			await player.extractors.loadMulti(DefaultExtractors);
 
-		const voiceChannel = interaction.member.voice.channel;
-		if (!voiceChannel) {
-			await interaction.reply('You must be in a voice channel to play music!');
-			return;
+			const voiceChannel = interaction.member.voice.channel;
+			if (!voiceChannel) {
+				await interaction.reply('You must be in a voice channel to play music!');
+				return;
+			}
+
+			const query = interaction.options.getString('song', true);
+			if (!query) {
+				await interaction.reply('You must provide a song name or URL to play!');
+				return;
+			}
+
+			await interaction.deferReply();
+
+			const searchResult = await player.search(query, {
+				requestedBy: interaction.user,
+				searchEngine: QueryType.AUTO
+			});
+
+			if (!searchResult.hasTracks()) {
+				await interaction.editReply(`We found no tracks for "${query}"!`);
+				return;
+			}
+
+			const track = searchResult.tracks[0];
+			const queue = await player.play(voiceChannel, searchResult, {
+				nodeOptions: {
+					metadata: interaction.channel,
+					volume: 50, 
+					leaveOnEmpty: true,
+					leaveOnEnd: false
+				},
+			});
+
+			const embed = new EmbedBuilder()
+				.setColor('#0099ff')
+				.setTitle('🎵 Now Playing')
+				.setDescription(`[${track.title}](${track.url})`)
+				.addFields(
+					{ name: 'Artist', value: track.author || 'Unknown', inline: true },
+					{ name: 'Duration', value: track.duration || 'Unknown', inline: true }
+				)
+				.setThumbnail(track.thumbnail);
+
+			await interaction.editReply({ 
+				content: 'Track added to queue!', 
+				embeds: [embed] 
+			});
+		} catch (error) {
+			console.error('Play command error:', error);
+			await interaction.editReply('An error occurred while trying to play the track.');
 		}
-
-		const query = interaction.options.getString('song', true);
-		if (!query) {
-			await interaction.reply('You must provide a song name or URL to play!');
-			return;
-		}
-
-		await interaction.deferReply();
-
-		const searchResult = await player.search(query, {
-			requestedBy: interaction.user,
-		});
-
-		if (!searchResult.hasTracks()) {
-			await interaction.editReply(`We found no tracks for "${query}"!`);
-			return;
-		}
-
-		await player.play(voiceChannel, searchResult, {
-			nodeOptions: {
-				metadata: interaction.channel,
-				// You can add more options here
-			},
-		});
-
-		await interaction.editReply({ content: `Loading your track(s)...` });
 	},
 };
