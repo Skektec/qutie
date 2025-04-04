b1c66eb3cddc5b657ccfd42a176ec65d8f42d9f1
Skek <skekung.yt@gmail.com>
2025-04-04 16:41:14 +0200

    Better error logging

--- events/errorLog.js
+++ events/errorLog.js
@@ -6,8 +6,9 @@ module.exports = {
 	execute: async (message) => {
 		const client = getClient();
 		const user = await client.users.fetch(botAdimn);
-		if (message < 2000) user.send(message);
-		else if (message > 2000) user.send('An error exceeded 2000 characters');
+		if (message.length < 2000) user.send(message);
+		else if (message.length > 2000)
+			user.send('An error exceeded 2000 characters');
 		else user.send('A blank error occured');
 	},
 };
