7108aebb4dffa9ba9bb89a84691384059d52ac52
Skekung <skekung.yt@gmail.com>
2025-05-31 20:00:51 +0200

    Added conditions

--- functions/removeTracker.js
+++ functions/removeTracker.js
@@ -2,25 +2,30 @@ module.exports = {
   execute: async (message) => {
     let cleaned = message.content;
 
-    cleaned = cleaned.replace(
-      /(https?:\/\/(?:www\.)?reddit\.com\/[^\s?]+)(\?utm[^\s]*)?/gi,
-      "$1"
-    );
+    if (
+      message.content.match(/x.com|reddit.com/i) |
+      message.content.match(/\?t=|\?utm_source/i)
+    ) {
+      cleaned = cleaned.replace(
+        /(https?:\/\/(?:www\.)?reddit\.com\/[^\s?]+)(\?utm[^\s]*)?/gi,
+        "$1"
+      );
 
-    cleaned = cleaned.replace(
-      /(https?:\/\/(?:www\.)?x\.com\/[^\s?]+)\?t=[^\s&]+[^\s]*/gi,
-      "$1"
-    );
+      cleaned = cleaned.replace(
+        /(https?:\/\/(?:www\.)?x\.com\/[^\s?]+)\?t=[^\s&]+[^\s]*/gi,
+        "$1"
+      );
 
-    cleaned = cleaned.replace(
-      /https?:\/\/(?:www\.)?x\.com/gi,
-      "https://fixupx.com"
-    );
-    cleaned = cleaned.replace(
-      /https?:\/\/(?:www\.)?reddit\.com/gi,
-      "https://rxddit.com"
-    );
+      cleaned = cleaned.replace(
+        /https?:\/\/(?:www\.)?x\.com/gi,
+        "https://fixupx.com"
+      );
+      cleaned = cleaned.replace(
+        /https?:\/\/(?:www\.)?reddit\.com/gi,
+        "https://rxddit.com"
+      );
 
-    return cleaned;
+      return cleaned;
+    }
   },
 };
