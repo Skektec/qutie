5a599d4d35dc6ecc5914fed7506d775288e3d4af
Skekung <skekung.yt@gmail.com>
2025-06-23 21:45:28 +0200

    suppport fixes

--- functions/support.js
+++ functions/support.js
@@ -1,24 +1,24 @@
-const { EmbedBuilder } = require('discord.js');
-const { getClient } = require('../data/clientInstance');
+const { EmbedBuilder } = require("discord.js");
+const { getClient } = require("../data/clientInstance");
 client = getClient();
 
 module.exports = {
-	answer: async (message, args) => {
-		const channelId = args[0];
-		const reply = args[1];
+  answer: async (message, args) => {
+    const channelId = args[0];
+    const reply = args.slice(1).join(" ");
 
-		const channel = await client.channels.fetch(channelId);
+    const channel = await client.channels.fetch(channelId);
 
-		try {
-			const supportEmbed = new EmbedBuilder()
-				.setColor(0x0099ff)
-				.setTitle('**Qutie Support**')
-				.addFields({ name: 'Reply:', value: reply })
-				.setFooter({ text: `Answered by ${message.author.username}` });
+    try {
+      const supportEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle("**Qutie Support**")
+        .addFields({ name: "Reply:", value: reply })
+        .setFooter({ text: `Answered by ${message.author.username}` });
 
-			channel.send({ embeds: [supportEmbed] });
-		} catch (err) {
-			message.channel.send(`Reply failed: ${err}`);
-		}
-	},
+      channel.send({ embeds: [supportEmbed] });
+    } catch (err) {
+      message.channel.send(`Reply failed: ${err}`);
+    }
+  },
 };
