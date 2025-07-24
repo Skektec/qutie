f901e5527bfc1009d354000816cadd333b9e1cbe
Skekung <skekung.yt@gmail.com>
2025-07-24 19:44:54 +0200

    increased embed suppress wait time to hopefully reduce failures

--- functions/removeTracker.js
+++ functions/removeTracker.js
@@ -1,29 +1,31 @@
 module.exports = {
-  execute: async (message) => {
-    function delay(time) {
-      return new Promise((resolve) => setTimeout(resolve, time));
-    }
+	execute: async (message) => {
+		function delay(time) {
+			return new Promise((resolve) => setTimeout(resolve, time));
+		}
 
-    let cleaned = message.content;
+		let cleaned = message.content;
 
-    if (message.content.includes("http")) {
-      cleaned = cleaned.replace(
-        /(https?:\/\/(?:www\.)?reddit\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
-        "https://rxddit.com$2"
-      );
+		if (message.content.includes('http')) {
+			cleaned = cleaned.replace(
+				/(https?:\/\/(?:www\.)?reddit\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
+				'https://rxddit.com$2'
+			);
 
-      cleaned = cleaned.replace(
-        /(https?:\/\/(?:www\.)?x\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
-        "https://fixupx.com$2"
-      );
+			cleaned = cleaned.replace(
+				/(https?:\/\/(?:www\.)?x\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
+				'https://fixupx.com$2'
+			);
 
-      if (cleaned === message.content) {
-        return;
-      } else {
-        delay(1000);
-        await message.suppressEmbeds(true);
-        return cleaned;
-      }
-    }
-  },
+			if (cleaned === message.content) {
+				return;
+			} else {
+				delay(1200);
+				await message.suppressEmbeds(true);
+				return cleaned;
+			}
+		} else {
+			return;
+		}
+	}
 };
