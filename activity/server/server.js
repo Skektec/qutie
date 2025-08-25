9ca2853a3a8d5efc886706cdd12f1eae067e3d76
Skekung <skekung.yt@gmail.com>
2025-08-25 22:50:47 +0200

    revert

--- activity/server/server.js
+++ activity/server/server.js
@@ -1,44 +1,36 @@
-import fs from 'fs';
-import https from 'https';
 import express from 'express';
 import fetch from 'node-fetch';
-import config from '../../data/config.json' assert { type: 'json' };
+import config from '../../data/config.json' with { type: 'json' };
+
 
 const app = express();
-const port = 443; // Discord requires HTTPS on 443
+const port = 3001;
 
 // Allow express to parse JSON bodies
 app.use(express.json());
 
 app.post('/api/token', async (req, res) => {
-	try {
-		const response = await fetch(`https://discord.com/api/oauth2/token`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			body: new URLSearchParams({
-				client_id: config.clientId,
-				client_secret: config.discordToken,
-				grant_type: 'authorization_code',
-				code: req.body.code
-			})
-		});
+	// Exchange the code for an access_token
+	const response = await fetch(`https://discord.com/api/oauth2/token`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: new URLSearchParams({
+			client_id: config.clientId,
+			client_secret: config.discordToken,
+			grant_type: 'authorization_code',
+			code: req.body.code
+		})
+	});
 
-		const data = await response.json();
+	// Retrieve the access_token from the response
+	const { access_token } = await response.json();
 
-		res.send({ access_token: data.access_token });
-	} catch (err) {
-		console.error('Token exchange failed', err);
-		res.status(500).send('OAuth exchange failed');
-	}
+	// Return the access_token to our client as { access_token: "..."}
+	res.send({ access_token });
 });
 
-const options = {
-	key: fs.readFileSync('/etc/letsencrypt/live/fluxus.ddns.net/privkey.pem'),
-	cert: fs.readFileSync('/etc/letsencrypt/live/fluxus.ddns.net/fullchain.pem')
-};
-
-https.createServer(options, app).listen(port, () => {
-	console.log(`✅ HTTPS server running at https://fluxus.ddns.net:${port}`);
+app.listen(port, () => {
+	console.log(`Server listening at http://0.0.0.0:${port}`);
 });
