f3ce3c0293f85f2170f03d9e857e72763b6ea314
Skekung <skekung.yt@gmail.com>
2025-10-02 18:45:58 +0200

    added search term if it fails

--- jarvis_commands/gif.js
+++ jarvis_commands/gif.js
@@ -1,30 +1,30 @@
-const { tenorToken } = require("../data/config.json");
-const notify = require("../functions/notify");
+const { tenorToken } = require('../data/config.json');
+const notify = require('../functions/notify');
 
 module.exports = {
-  execute: async (match, message) => {
-    const searchTerm = match[1];
+	execute: async (match, message) => {
+		const searchTerm = match[1];
 
-    try {
-      const fetch = (await import("node-fetch")).default;
+		try {
+			const fetch = (await import('node-fetch')).default;
 
-      const response = await fetch(
-        `https://tenor.googleapis.com/v2/search?q=${encodeURIComponent(
-          searchTerm
-        )}&key=${tenorToken}&limit=1`
-      );
+			const response = await fetch(
+				`https://tenor.googleapis.com/v2/search?q=${encodeURIComponent(
+					searchTerm
+				)}&key=${tenorToken}&limit=1`
+			);
 
-      const data = await response.json();
+			const data = await response.json();
 
-      if (data.results && data.results.length > 0) {
-        const gifUrl = data.results[0].url;
-        message.channel.send(gifUrl);
-      } else {
-        message.channel.send("No GIFs found for your search term.");
-      }
-    } catch (error) {
-      notify.error("Error fetching GIF:", error);
-      message.reply("An error occurred while fetching the GIF.");
-    }
-  },
+			if (data.results && data.results.length > 0) {
+				const gifUrl = data.results[0].url;
+				message.channel.send(gifUrl);
+			} else {
+				message.channel.send(`No GIFs found for your search term; "${searchTerm}"`);
+			}
+		} catch (error) {
+			notify.error('Error fetching GIF:', error);
+			message.reply('An error occurred while fetching the GIF.');
+		}
+	}
 };
