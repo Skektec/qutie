bae32293bbe73a2975bed41bb37f9598446c5634
Skekung <skekung.yt@gmail.com>
2025-10-15 13:46:18 +0200

    added message encryption

--- functions/storeMessage.js
+++ functions/storeMessage.js
@@ -1,18 +1,38 @@
 const db = require('./database.js');
+const crypto = require('crypto');
+require('dotenv').config();
+
+const algorithm = 'aes-256-gcm';
+const keyHex = process.env.STORING - STRING;
 
 module.exports = {
 	save: async (message) => {
+		if (!keyHex) {
+			throw new Error('STORING-STRING is not set in the .env file.');
+		}
+
+		function encrypt(text) {
+			const iv = crypto.randomBytes(16);
+			const cipher = crypto.createCipheriv(algorithm, key, iv);
+			let encrypted = cipher.update(text, 'utf8', 'hex');
+			encrypted += cipher.final('hex');
+			const authTag = cipher.getAuthTag();
+			return `${iv.toString('hex')}:${authTag.toString('hex')}:${encrypted}`;
+		}
+
 		const queryText = `
             INSERT INTO "MessageStore" (nick, userId, channel, server, text, messageId, time)
             VALUES ($1, $2, $3, $4, $5, $6, $7)
         `;
+
+		const EncryptedMessage = encrypt(message.content);
 
 		const values = [
 			message.author.username,
 			message.author.id,
 			message.channel.id,
 			message.guild.id,
-			message.content,
+			EncryptedMessage,
 			message.id,
 			message.createdTimestamp
 		];
