33c1f14ce1a398d43f94efa53094b8c42fea0e4b
Skekung <skekung.yt@gmail.com>
2025-10-15 13:31:18 +0200

    mvoed epics

--- bot.js
+++ bot.js
@@ -8,15 +8,15 @@ const { setClient } = require('./data/clientInstance');
 const database = require('./functions/database');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildVoiceStates
-  ],
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildVoiceStates
+	],
+	partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
 //Stores the client instance.
@@ -30,76 +30,76 @@ const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
 }
 
 for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
 
 cron.schedule('0 13 * * *', async () => {
-  try {
-    const today = new Date();
-    const currentDate = `${today.getDate()}-${today.toLocaleString('en-US', { month: 'long' })}`;
+	try {
+		const today = new Date();
+		const currentDate = `${today.getDate()}-${today.toLocaleString('en-US', { month: 'long' })}`;
 
-    database.query(
-      `SELECT * FROM birthdays WHERE date LIKE '%${currentDate}%'`,
-      async (err, result) => {
-        if (err) {
-          notify.error('Error querying the database:', err, '1x36079');
-          return;
-        }
+		database.query(
+			`SELECT * FROM birthdays WHERE date LIKE '%${currentDate}%'`,
+			async (err, result) => {
+				if (err) {
+					notify.error('Error querying the database:', err, '1x36079');
+					return;
+				}
 
-        const rows = result.rows;
+				const rows = result.rows;
 
-        if (rows.length === 0) {
-          return;
-        }
+				if (rows.length === 0) {
+					return;
+				}
 
-        for (const row of rows) {
-          try {
-            const birthdayUser = row.id && row.id !== '0' ? `<@${row.id}>` : row.nick || 'unknown';
-            const channel = await client.channels.fetch(row.channel);
+				for (const row of rows) {
+					try {
+						const birthdayUser = row.id && row.id !== '0' ? `<@${row.id}>` : row.nick || 'unknown';
+						const channel = await client.channels.fetch(row.channel);
 
-            if (!channel) {
-              console.log(`Channel ${row.channel} not found`);
-              continue;
-            }
+						if (!channel) {
+							console.log(`Channel ${row.channel} not found`);
+							continue;
+						}
 
-            channel.send(`🎉 Happy Birthday ${birthdayUser}! 🎉`);
-            return;
-          } catch (channelError) {
-            notify.error(
-              `Error fetching channel ${row.channel} or id ${row.id} or nick ${row.nick}:`,
-              channelError,
-              '4x36104'
-            );
-            return;
-          }
-        }
-      }
-    );
-  } catch (err) {
-    notify.error('Error displaying birthday:', err, '2x36112');
-  }
+						channel.send(`🎉 Happy Birthday ${birthdayUser}! 🎉`);
+						return;
+					} catch (channelError) {
+						notify.error(
+							`Error fetching channel ${row.channel} or id ${row.id} or nick ${row.nick}:`,
+							channelError,
+							'4x36104'
+						);
+						return;
+					}
+				}
+			}
+		);
+	} catch (err) {
+		notify.error('Error displaying birthday:', err, '2x36112');
+	}
 });
 
 client.login(discordToken);
