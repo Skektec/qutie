a835e70310d7b082a6ca308107ee1ab52bcb5718
Skekung <skekung.yt@gmail.com>
2025-10-16 14:09:40 +0200

    deploymentNotice for github action

---
+++ events/Actions/deploymentNotice.js
@@ -0,0 +1,16 @@
+const { Client, Partials } = require('discord.js');
+const { botAdimn, discordToken } = require('../../data/config.json');
+
+const client = new Client({
+	intents: [],
+	partials: [Partials.Message, Partials.Channel]
+});
+
+async function deploymentNotice() {
+	client.login(discordToken);
+	const user = await client.users.fetch(botAdimn);
+	await user.send('Deployed new code successfully.');
+	process.exit(1);
+}
+
+deploymentNotice();
