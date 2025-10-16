52158924244232cfecbadc5531767473ca1cd4b0
Skekung <skekung.yt@gmail.com>
2025-10-16 16:08:49 +0200

    uiwjebfgi

--- events/Actions/deploymentNotice.js
+++ events/Actions/deploymentNotice.js
@@ -12,11 +12,9 @@ async function deploymentNotice() {
 			const user = await client.users.fetch(botAdimn);
 			await user.send('Deployed new code successfully.');
 
-			client.destroy();
 			process.exit(0);
 		} catch (err) {
 			console.error('ERROR during message send/fetch:', err);
-			client.destroy();
 			process.exit(1);
 		}
 	});
