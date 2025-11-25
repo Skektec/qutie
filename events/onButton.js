376a965a292bd877f54f05c1caba1f16926ecb6c
Skekung <skekung.yt@gmail.com>
2025-11-25 13:00:48 +0200

    replaced if statements in onMessage event handler to use switches

--- events/onButton.js
+++ events/onButton.js
@@ -1,164 +1,164 @@
 const maps = require('../data/wtMaps.json');
-const { MessageFlags } = require('discord.js');
-const { teamDataMap, getMap, formTeam, winner, rollTeam } = require('../commands/games/formTeam');
+const {MessageFlags} = require('discord.js');
+const {teamDataMap, getMap, formTeam, winner, rollTeam} = require('../commands/games/formTeam');
 
 const gameFiles = {
-	warthunder: require('../games/warthunder'),
-	nebulous: require('../games/nebulous')
+    warthunder: require('../games/warthunder'),
+    nebulous: require('../games/nebulous')
 };
 
 playerRecordArray = [];
 
 module.exports = {
-	name: 'interactionCreate',
-	async execute(buttonInteraction) {
-		try {
-			if (!buttonInteraction.isButton()) return;
+    name: 'interactionCreate',
+    async execute(buttonInteraction) {
+        try {
+            if (!buttonInteraction.isButton()) return;
 
-			const fetchedMessage = buttonInteraction.message;
-			const teamData = teamDataMap.get(buttonInteraction.message.id);
+            const fetchedMessage = buttonInteraction.message;
+            const teamData = teamDataMap.get(buttonInteraction.message.id);
 
-			let {
-				team1,
-				team2,
-				playerRecordArray = [],
-				randomMapName,
-				randomMapUrl,
-				gameName = 'warthunder'
-			} = teamData;
+            let {
+                team1,
+                team2,
+                playerRecordArray = [],
+                randomMapName,
+                randomMapUrl,
+                gameName = 'warthunder'
+            } = teamData;
 
-			isWinner = 3;
+            isWinner = 3;
 
-			const gameModule = gameFiles[gameName];
+            const gameModule = gameFiles[gameName];
 
-			if (!gameModule) {
-				throw new Error(`Game module not found: ${gameName}`);
-			}
+            if (!gameModule) {
+                throw new Error(`Game module not found: ${gameName}`);
+            }
 
-			const userId = buttonInteraction.user.id;
+            const userId = buttonInteraction.user.id;
 
-			try {
-				await buttonInteraction.deferUpdate();
-			} catch (deferError) {
-				console.error('Error deferring update:', deferError);
-			}
+            try {
+                await buttonInteraction.deferUpdate();
+            } catch (deferError) {
+                console.error('Error deferring update:', deferError);
+            }
 
-			if (buttonInteraction.customId === 'newmap') {
-				const newMapData = getMap(maps);
+            if (buttonInteraction.customId === 'newmap') {
+                const newMapData = getMap(maps);
 
-				teamData.randomMapName = newMapData.randomMapName;
-				teamData.randomMapUrl = newMapData.randomMapUrl;
+                teamData.randomMapName = newMapData.randomMapName;
+                teamData.randomMapUrl = newMapData.randomMapUrl;
 
-				const updatedTeam = gameModule.updateTeamEmbed(
-					team1,
-					team2,
-					newMapData.randomMapName,
-					newMapData.randomMapUrl
-				);
-				await fetchedMessage.edit({ embeds: [updatedTeam] });
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(
+                    team1,
+                    team2,
+                    newMapData.randomMapName,
+                    newMapData.randomMapUrl
+                );
+                await fetchedMessage.edit({embeds: [updatedTeam]});
+            }
 
-			if (buttonInteraction.customId === 'team1Win') {
-				winner(team1, team2, 1, gameName);
+            if (buttonInteraction.customId === 'team1Win') {
+                winner(team1, team2, 1, gameName);
 
-				isWinner = 1;
+                isWinner = 1;
 
-				const updatedTeam = gameModule.updateTeamEmbed(
-					team1,
-					team2,
-					randomMapName,
-					randomMapUrl,
-					1
-				);
-				const [row1, row2] = gameModule.createTeamButtons(true);
-				await fetchedMessage.edit({
-					embeds: [updatedTeam],
-					components: [row1, row2]
-				});
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(
+                    team1,
+                    team2,
+                    randomMapName,
+                    randomMapUrl,
+                    1
+                );
+                const [row1, row2] = gameModule.createTeamButtons(true);
+                await fetchedMessage.edit({
+                    embeds: [updatedTeam],
+                    components: [row1, row2]
+                });
+            }
 
-			if (buttonInteraction.customId === 'team2Win') {
-				winner(team1, team2, 2, gameName);
+            if (buttonInteraction.customId === 'team2Win') {
+                winner(team1, team2, 2, gameName);
 
-				isWinner = 2;
+                isWinner = 2;
 
-				const updatedTeam = gameModule.updateTeamEmbed(
-					team1,
-					team2,
-					randomMapName,
-					randomMapUrl,
-					2
-				);
-				const [row1, row2] = gameModule.createTeamButtons(true);
-				await fetchedMessage.edit({
-					embeds: [updatedTeam],
-					components: [row1, row2]
-				});
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(
+                    team1,
+                    team2,
+                    randomMapName,
+                    randomMapUrl,
+                    2
+                );
+                const [row1, row2] = gameModule.createTeamButtons(true);
+                await fetchedMessage.edit({
+                    embeds: [updatedTeam],
+                    components: [row1, row2]
+                });
+            }
 
-			if (buttonInteraction.customId === 'noWin') {
-				isWinner = 0;
+            if (buttonInteraction.customId === 'noWin') {
+                isWinner = 0;
 
-				const updatedTeam = gameModule.updateTeamEmbed(
-					team1,
-					team2,
-					randomMapName,
-					randomMapUrl,
-					0
-				);
-				const [row1, row2] = gameModule.createTeamButtons(true);
-				await fetchedMessage.edit({
-					embeds: [updatedTeam],
-					components: [row1, row2]
-				});
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(
+                    team1,
+                    team2,
+                    randomMapName,
+                    randomMapUrl,
+                    0
+                );
+                const [row1, row2] = gameModule.createTeamButtons(true);
+                await fetchedMessage.edit({
+                    embeds: [updatedTeam],
+                    components: [row1, row2]
+                });
+            }
 
-			if (buttonInteraction.customId === 'join') {
-				const { team1, team2 } = await formTeam(userId, gameName, playerRecordArray);
+            if (buttonInteraction.customId === 'join') {
+                const {team1, team2} = await formTeam(userId, gameName, playerRecordArray);
 
-				teamData.team1 = team1;
-				teamData.team2 = team2;
+                teamData.team1 = team1;
+                teamData.team2 = team2;
 
-				const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
-				await fetchedMessage.edit({ embeds: [updatedTeam] });
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
+                await fetchedMessage.edit({embeds: [updatedTeam]});
+            }
 
-			if (buttonInteraction.customId === 'leave') {
-				const index1 = team1.findIndex((member) => member === userId);
-				if (index1 !== -1) team1.splice(index1, 1);
+            if (buttonInteraction.customId === 'leave') {
+                const index1 = team1.findIndex((member) => member === userId);
+                if (index1 !== -1) team1.splice(index1, 1);
 
-				const index2 = team2.findIndex((member) => member === userId);
-				if (index2 !== -1) team2.splice(index2, 1);
+                const index2 = team2.findIndex((member) => member === userId);
+                if (index2 !== -1) team2.splice(index2, 1);
 
-				const recordIndex = playerRecordArray.findIndex((record) => record.hasOwnProperty(userId));
-				if (recordIndex !== -1) {
-					playerRecordArray.splice(recordIndex, 1);
-				}
+                const recordIndex = playerRecordArray.findIndex((record) => record.hasOwnProperty(userId));
+                if (recordIndex !== -1) {
+                    playerRecordArray.splice(recordIndex, 1);
+                }
 
-				teamData.playerRecordArray = playerRecordArray;
+                teamData.playerRecordArray = playerRecordArray;
 
-				const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
-				await fetchedMessage.edit({ embeds: [updatedTeam] });
-			}
+                const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
+                await fetchedMessage.edit({embeds: [updatedTeam]});
+            }
 
-			if (buttonInteraction.customId === 'rollTeam') {
-				if (winner == 3) {
-					const { team1, team2 } = await rollTeam(playerRecordArray);
+            if (buttonInteraction.customId === 'rollTeam') {
+                if (winner == 3) {
+                    const {team1, team2} = await rollTeam(playerRecordArray);
 
-					const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
-					await fetchedMessage.edit({ embeds: [updatedTeam] });
-				} else return;
-			}
-		} catch (error) {
-			console.error('Unhandled error in button interaction:', error);
-			try {
-				await buttonInteraction.reply({
-					content: 'An error occurred while processing your interaction.',
-					flags: MessageFlags.Ephemeral
-				});
-			} catch (replyError) {
-				console.error('Error sending error reply:', replyError);
-			}
-		}
-	}
+                    const updatedTeam = gameModule.updateTeamEmbed(team1, team2, randomMapName, randomMapUrl);
+                    await fetchedMessage.edit({embeds: [updatedTeam]});
+                }
+            }
+        } catch (error) {
+            console.error('Unhandled error in button interaction:', error);
+            try {
+                await buttonInteraction.reply({
+                    content: 'An error occurred while processing your interaction.',
+                    flags: MessageFlags.Ephemeral
+                });
+            } catch (replyError) {
+                console.error('Error sending error reply:', replyError);
+            }
+        }
+    }
 };
--- events/onMessage.js
+++ events/onMessage.js
@@ -1,6 +1,6 @@
-const { Events, MessageFlags, ClientApplication } = require('discord.js');
-const { botAdimn } = require('../data/config.json');
-const { nvmGif, neverKysVideo, zLink } = require('../data/pubconfig.js');
+const {Events, MessageFlags, ClientApplication} = require('discord.js');
+const {botAdimn} = require('../data/config.json');
+const {nvmGif, neverKysVideo, zLink} = require('../data/pubconfig.js');
 // const mutedUsers = require('../data/mutedUsers.json');
 const fetchquote = require('../functions/fetchquote');
 const notify = require('../functions/notify');
@@ -16,187 +16,192 @@ const fetchDev = require('../functions/warThunder/fetchDev.js');
 const fetchNews = require('../functions/warThunder/fetchNews.js');
 
 // const repostDetection = require('../functions/repostDetection');
-const { exec } = require('child_process');
-const { json } = require('stream/consumers');
+const {exec} = require('child_process');
+const {json} = require('stream/consumers');
 
 const words = ['uwu', 'owo', 'blahaj'];
 
 function delay(time) {
-	return new Promise((resolve) => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 const prevMessages = [];
 
 module.exports = {
-	name: Events.MessageCreate,
-	execute: async (message) => {
-		jarvis.execute(message);
+    name: Events.MessageCreate,
+    execute: async (message) => {
 
-		if (message.content.startsWith('runTest')) {
-			// message.reply({
-			// 	content: `No test to execute.`,
-			// 	flags: MessageFlags.Ephemeral
-			// });'
-			// fetchDev.findLinks();
-			// fetchNews.findLinks();
-		}
+        if (message.author.bot) return;
+        jarvis.execute(message);
+        if (message.channel === 1200118011806367825) return;
+        prevMessages.push(message.content);
+        if (prevMessages.length > 3) prevMessages.shift();
+        if (words.includes(message.content)) sendEmoji.execute(message);
+        if (message.tts === true) {
+            await message.reply('kys');
+            message.delete();
+        }
 
-		if (message.channel == 1200118011806367825) return;
+        switch (message) {
+            case (message.content.startsWith('runTest')):
+                // message.reply({
+                // 	content: `No test to execute.`,
+                // 	flags: MessageFlags.Ephemeral
+                // });'
+                // fetchDev.findLinks();
+                // fetchNews.findLinks();
+                break;
 
-		// if (message.guild && message.guild.id === '973484576703905802') storeMessage.save(message);
+            case (message.content.startsWith('.answer') && message.author.id === botAdimn):
+                support.answer(message, message.content.slice(7).trim().split(/ +/));
+                break;
 
-		if (message.author.bot) return;
+            case (message.content.toLowerCase().startsWith('.q')):
+                if (message.content.toLowerCase().startsWith('.q help')) {
+                    message.reply("- /addquote - Manually add a quote.\n- /deletequote - Manually delete a quote.\n- /quoteboard - Creates a leaderboard with the amount of quotes from each user in the server.\n- /searchquote - Search for specific keywords in quotes.")
+                } else {
+                    fetchquote.execute(message, message.content.slice(2).trim().split(/ +/));
+                }
+                break;
 
-		prevMessages.push(message.content);
+            case (message.content.startsWith('!p')):
+                //For running python commands, seems to be broken but is unused.
 
-		if (prevMessages.length > 3) prevMessages.shift();
+                const args = message.content.slice(3).trim().split(' ');
+                const command = args.shift();
+                const extraArguments = args.join(' ');
 
-		if (words.includes(message.content)) sendEmoji.execute(message);
+                const commandFilePath = path.join(__dirname, 'commands', 'python_commands', `${command}.py`);
 
-		if (message.tts === true) {
-			await message.reply('kys');
-			message.delete();
-		}
-		if (message.content.match(/—/g)) {
-			await message.reply('em dash detected\nclanka clanka clanka');
-		}
+                fs.access(commandFilePath, fs.constants.F_OK, (err) => {
+                    if (err) {
+                        message.reply(`Command "${command}" not found.`);
+                        return;
+                    }
 
-		if (
-			prevMessages.length === 3 &&
-			prevMessages.every((msg) => msg === prevMessages[0] && !message.content.startsWith('.q'))
-		) {
-			message.channel.send(prevMessages[2]);
-		}
+                    if (!extraArguments.trim()) {
+                        exec(`python "${commandFilePath}"`, (error, stdout, stderr) => {
+                            if (error) {
+                                message.reply(`Error: ${error.message} (Occured in python code)`);
+                                return;
+                            }
+                            if (stderr) {
+                                message.reply(`Python Error: ${stderr}`);
+                                return;
+                            }
 
-		if (message.content.match(/kms|kill myself|killing myself/i)) {
-			message.reply({
-				content: neverKysVideo,
-				allowedMentions: { repliedUser: false }
-			});
-		}
+                            const response = stdout.trim();
 
-		// if (lowerCaseMessage.match(/zov/)) {
-		// 	if (message.content.match('https://')) return;
-		// 	const zMessage = message.content.replace(/c/g, 'z').replace(/s/g, 'Z');
-		// 	message.channel.send(`${zMessage} 🇷🇺🇷🇺🇷🇺`);
-		// 	message.channel.send('ZZZ');
-		// 	delay(400);
-		// 	message.channel.send('Z🇷🇺Z🇷🇺Z');
-		// 	delay(400);
-		// 	message.channel.send('ZZ');
-		// 	message.channel.send(`${zLink}`);
-		// }
+                            if (response === '') {
+                                message.reply('No response from the command.');
+                            } else {
+                                message.reply(response);
+                            }
+                        });
+                    } else {
+                        exec(`python "${commandFilePath}" "${extraArguments}"`, (error, stdout, stderr) => {
+                            if (error) {
+                                message.reply(`Error: ${error.message}  (Occured in python code)`);
+                                return;
+                            }
+                            if (stderr) {
+                                message.reply(`Python Error: ${stderr}`);
+                                return;
+                            }
 
-		if (
-			message.content.match(
-				/https:\/\/x.com|https:\/\/reddit.com|https:\/\/www.reddit.com|https:\/\/instagram.com/i
-			)
-		) {
-			const cleanLink = await clean.execute(message);
-			if (cleanLink)
-				message.reply({
-					content: cleanLink,
-					allowedMentions: { repliedUser: false }
-				});
-			delay(2000);
-			await message.suppressEmbeds(true);
-			return;
-		}
+                            const response = stdout.trim();
 
-		if (message.content.startsWith('.answer') && message.author.id === botAdimn) {
-			support.answer(message, message.content.slice(7).trim().split(/ +/));
-		}
+                            if (response === '') {
+                                message.reply('No response from the command.');
+                            } else {
+                                message.reply(response);
+                            }
+                        });
+                    }
+                });
+                break;
 
-		if (message.content.toLowerCase().match(/^ok garmin,? video speichern/)) {
-			const messageReply = await message.channel.messages.fetch(message.reference.messageId);
-			messageAddQuote.execute(messageReply, message.author);
-		}
+            case (
+                prevMessages.length === 3 &&
+                prevMessages.every((msg) => msg === prevMessages[0] && !message.content.startsWith('.q'))
+            ):
+                message.channel.send(prevMessages[2]);
+                break;
 
-		if (message.content.toLowerCase().startsWith('.q')) {
-			if (message.content.toLowerCase().startsWith('.q help')) {
-				message.reply("- /addquote - Manually add a quote.\n- /deletequote - Manually delete a quote.\n- /quoteboard - Creates a leaderboard with the amount of quotes from each user in the server.\n- /searchquote - Search for specific keywords in quotes.")
-			} else {
-				fetchquote.execute(message, message.content.slice(2).trim().split(/ +/));
-			}
-		}
+            default:
+                break;
+        }
 
-		if (message.content.startsWith('!p')) {
-			const args = message.content.slice(3).trim().split(' ');
-			const command = args.shift();
-			const extraArguments = args.join(' ');
+        switch (message) {
+            case (message.content.match(/kms|kill myself|killing myself/i)):
+                message.reply({
+                    content: neverKysVideo,
+                    allowedMentions: {repliedUser: false}
+                });
+                break;
 
-			const commandFilePath = path.join(__dirname, 'commands', 'python_commands', `${command}.py`);
+            case message.content.match(
+                /https:\/\/x.com|https:\/\/reddit.com|https:\/\/www.reddit.com|https:\/\/instagram.com/i
+            ):
+                const cleanLink = await clean.execute(message);
+                if (cleanLink)
+                    message.reply({
+                        content: cleanLink,
+                        allowedMentions: {repliedUser: false}
+                    });
+                delay(2000);
+                await message.suppressEmbeds(true);
+                break;
 
-			fs.access(commandFilePath, fs.constants.F_OK, (err) => {
-				if (err) {
-					message.reply(`Command "${command}" not found.`);
-					return;
-				}
+            case (message.content.toLowerCase().match(/^ok garmin,? video speichern/)):
+                const messageReply = await message.channel.messages.fetch(message.reference.messageId);
+                messageAddQuote.execute(messageReply, message.author);
+                break;
 
-				if (!extraArguments.trim()) {
-					exec(`python "${commandFilePath}"`, (error, stdout, stderr) => {
-						if (error) {
-							message.reply(`Error: ${error.message} (Occured in python code)`);
-							return;
-						}
-						if (stderr) {
-							message.reply(`Python Error: ${stderr}`);
-							return;
-						}
+            case (message.content.toLowerCase().match(/zov/)):
+                // if (message.content.match('https://')) return;
+                // const zMessage = message.content.replace(/c/g, 'z').replace(/s/g, 'Z');
+                // message.channel.send(`${zMessage} 🇷🇺🇷🇺🇷🇺`);
+                // message.channel.send('ZZZ');
+                // delay(400);
+                // message.channel.send('Z🇷🇺Z🇷🇺Z');
+                // delay(400);
+                // message.channel.send('ZZ');
+                // message.channel.send(`${zLink}`);
+                break;
 
-						const response = stdout.trim();
+            case (message.content.match(/—/g)):
+                await message.reply('em dash detected\nclanka clanka clanka');
+                break;
 
-						if (response === '') {
-							message.reply('No response from the command.');
-						} else {
-							message.reply(response);
-						}
-					});
-				} else {
-					exec(`python "${commandFilePath}" "${extraArguments}"`, (error, stdout, stderr) => {
-						if (error) {
-							message.reply(`Error: ${error.message}  (Occured in python code)`);
-							return;
-						}
-						if (stderr) {
-							message.reply(`Python Error: ${stderr}`);
-							return;
-						}
+            default:
+                break;
+        }
 
-						const response = stdout.trim();
+        // if (message.guild && message.guild.id === '973484576703905802') storeMessage.save(message);
 
-						if (response === '') {
-							message.reply('No response from the command.');
-						} else {
-							message.reply(response);
-						}
-					});
-				}
-			});
-		}
+        // unreliable
 
-		// unreliable
+        // if (message.embeds.length > 0) {
+        // 	repostDetection.execute(message);
+        // }
 
-		// if (message.embeds.length > 0) {
-		// 	repostDetection.execute(message);
-		// }
+        // Just messing around with a mute function that deletes messages, not implemented.
+        // if (Array.isArray(mutedUsers) && mutedUsers.includes(message.author.id)) {
+        // 	message.reply({
+        // 		content: 'You are muted.',
+        // 		flags: MessageFlags.Ephemeral,
+        // 	});
+        //
+        // 	message.delete();
+        // 	return;
+        // }
 
-		// Just messing around with a mute function that deletes messages, not implemented.
-		// if (Array.isArray(mutedUsers) && mutedUsers.includes(message.author.id)) {
-		// 	message.reply({
-		// 		content: 'You are muted.',
-		// 		flags: MessageFlags.Ephemeral,
-		// 	});
-		//
-		// 	message.delete();
-		// 	return;
-		// }
-
-		// if (message.content === "nvm") {
-		//   message.reply({
-		//     content: nvmGif,
-		//     allowedMentions: { repliedUser: false },
-		//   });
-		// }
-	}
+        // if (message.content === "nvm") {
+        //   message.reply({
+        //     content: nvmGif,
+        //     allowedMentions: { repliedUser: false },
+        //   });
+        // }
+    }
 };
