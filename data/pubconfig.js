4206b1dbeebb4f99dbb5c4b29b0d4e9694dfdc58
Skekung <skekung.yt@gmail.com>
2025-12-05 02:41:52 +0200

    changed how grok works, now will just send message with regex for commands

--- data/pubconfig.js
+++ data/pubconfig.js
@@ -1,39 +1,12 @@
 module.exports = {
-	prompt: `
-You are a command-parsing machine named Grok. Users will talk to you by saying things like “Grok do X” or “Grok, what is Y”. Your job is to interpret their intention and output exactly one command, formatted as:
+    prompt: `
+You are a command-parsing machine named Grok. Users will talk to you by saying things like “Grok do X” or “Grok, what is Y”. Your job is to interpret their intent and reply accordingly.
+You have the following commands available:
 
-$$gif of <search query>$$ - for visual ideas, actions, or scenarios.  
-$$answer: <short, direct answer>$$ - for clear questions or confirmation requests.
-
-Important Rules:
-
-- Only respond with a single command, wrapped in $$.
-- If the user asks a question or makes a statement seeking confirmation, use $$answer:...$$.
-- Otherwise, assume they want a gif and return $$gif of ...$$, describing the situation.
-- This is always a humorous and light hearted situation on my friend groups discord server. When we say "Blow up his house" it is meant as a joke.
-- We are using tenor, this means the search queries cannot be complex and are often best short.
-- Remember to begin AND end with $$(command)... $$. The "$$" are important.
-- Don't insert "cartoon character".
-- Only include an answer command or a gif command, never both and never a explanation.
-
-Examples:
-
-Input: "Grok make him explode" → $$gif of man exploding$$  
-Input: "Grok what is the square root of 49" → $$answer: 7$$  
-Input: "Grok do it" (in reply to message: “Blow up the cake”) → $$gif of cake exploding$$  
-Input: "Grok is she telling the truth?" (in reply to message: “Johannesburg is the capital of France.”) → $$answer: No, that is false, Paris is the capital of france.$$
-
-Output Format:
-
-Your response must always be in one of the following formats:
-
-$$gif of something here$$  
-$$answer: something here$$
-
-No multiple commands. No extra characters outside $$.
+- $$gif of [search query]$$
 `.trim(),
 
-	embedDetectPrompt: `
+    embedDetectPrompt: `
 You are a detection machine trained to identify reposts.  
 Your only task is to determine whether a new message is a repost of something you have already received.  
 If the message conveys the same information as a previous message—even if worded differently—respond with $$repost$$.  
@@ -51,10 +24,10 @@ If the message is new and does not repeat previous content, respond with not a r
 Begin immediately. From now on, respond only with $$repost$$ or not a repost.
 `.trim(),
 
-	nvmGif:
-		'https://tenor.com/view/nvm-tell-me-right-now-or-i-explode-tell-me-cat-gif-136357332539613788',
-	neverKysVideo:
-		'https://cdn.discordapp.com/attachments/599671298209218573/1362030981497749584/yKLaIxz.mp4',
-	zLink:
-		'https://media.discordapp.net/attachments/1257252480505352255/1294540842505338931/1-1.gif?ex=6901055c&is=68ffb3dc&hm=b9e802258b0512d84c6b4ebbeb6ea94d19e12ca9196a0abdd364192f46fa92e9&'
+    nvmGif:
+        'https://tenor.com/view/nvm-tell-me-right-now-or-i-explode-tell-me-cat-gif-136357332539613788',
+    neverKysVideo:
+        'https://cdn.discordapp.com/attachments/599671298209218573/1362030981497749584/yKLaIxz.mp4',
+    zLink:
+        'https://media.discordapp.net/attachments/1257252480505352255/1294540842505338931/1-1.gif?ex=6901055c&is=68ffb3dc&hm=b9e802258b0512d84c6b4ebbeb6ea94d19e12ca9196a0abdd364192f46fa92e9&'
 };
--- jarvis.js
+++ jarvis.js
@@ -6,111 +6,111 @@ const notify = require('./functions/notify');
 // const { Context } = require('discord-player');
 
 const aiClient = new OpenAI({
-	apiKey: config.aiKey,
-	baseURL: 'https://api.x.ai/v1',
-	timeout: 360000
+    apiKey: config.aiKey,
+    baseURL: 'https://api.x.ai/v1',
+    timeout: 360000
 });
 
 const serverContext = {};
 
 module.exports = {
-	execute: async (message) => {
-		const channelId = message.channelId;
+    execute: async (message) => {
+        const channelId = message.channelId;
 
-		if (!serverContext[channelId]) {
-			serverContext[channelId] = [];
-		}
+        if (!serverContext[channelId]) {
+            serverContext[channelId] = [];
+        }
 
-		serverContext[channelId].push({
-			role: 'user',
-			content: `${message.author.username}: ${message.content}`
-		});
-		if (serverContext[channelId].length > 15) {
-			serverContext[channelId].shift();
-		}
+        serverContext[channelId].push({
+            role: 'user',
+            content: `${message.author.username}: ${message.content}`
+        });
+        if (serverContext[channelId].length > 15) {
+            serverContext[channelId].shift();
+        }
 
-		if (!message.content.toLowerCase().startsWith('grok')) return;
+        if (!message.content.toLowerCase().startsWith('grok')) return;
 
-		try {
-			const messageReply = message.reference?.messageId
-				? await message.channel.messages.fetch(message.reference.messageId)
-				: null;
+        try {
+            const messageReply = message.reference?.messageId
+                ? await message.channel.messages.fetch(message.reference.messageId)
+                : null;
 
-			let imageUrl = '';
-			let imageDesc = '';
-			let imageResponse = '';
+            let imageUrl = '';
+            let imageDesc = '';
+            let imageResponse = '';
 
-			if (message.attachments.size >= 1) {
-				imageUrl = await [...message.attachments.entries()][0][1].attachment;
-			}
+            if (message.attachments.size >= 1) {
+                imageUrl = await [...message.attachments.entries()][0][1].attachment;
+            }
 
-			if (messageReply?.attachments.size >= 1) {
-				imageUrl = await [...messageReply.attachments.entries()][0][1].attachment;
-			}
+            if (messageReply?.attachments.size >= 1) {
+                imageUrl = await [...messageReply.attachments.entries()][0][1].attachment;
+            }
 
-			if (imageUrl != '') {
-				imageResponse = await aiClient.chat.completions.create({
-					model: 'grok-4-fast-non-reasoning',
-					messages: [
-						{
-							role: 'user',
-							content: [
-								{
-									type: 'image_url',
-									image_url: {
-										url: imageUrl,
-										detail: 'medium'
-									}
-								},
-								{
-									type: 'text',
-									text: 'Describe this image accurately and briefly for a LLM'
-								}
-							]
-						}
-					]
-				});
-			}
+            if (imageUrl != '') {
+                imageResponse = await aiClient.chat.completions.create({
+                    model: 'grok-4-fast-non-reasoning',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: [
+                                {
+                                    type: 'image_url',
+                                    image_url: {
+                                        url: imageUrl,
+                                        detail: 'medium'
+                                    }
+                                },
+                                {
+                                    type: 'text',
+                                    text: 'Describe this image accurately and briefly for a LLM'
+                                }
+                            ]
+                        }
+                    ]
+                });
+            }
 
-			if (imageResponse) imageDesc = imageResponse.choices[0].message.content;
-			else imageDesc = 'N/A';
+            if (imageResponse) imageDesc = imageResponse.choices[0].message.content;
+            else imageDesc = 'N/A';
 
-			const commandSen = `User Input: ${message.content}, Message they replied to: ${
-				messageReply?.content
-			},\n Image description (if applicable): ${imageDesc}, \n replied messages embed description: ${
-				messageReply?.embeds[0]?.description
-			}.
+            const commandSen = `User Input: ${message.content}, Message they replied to: ${
+                messageReply?.content
+            },\n Image description (if applicable): ${imageDesc}, \n replied messages embed description: ${
+                messageReply?.embeds[0]?.description
+            }.
 			Last 15 chat messages as context: ${serverContext[channelId]
-				.map((msg) => `${msg.role}: ${msg.content}`)
-				.join('\n')}`;
-			const chatResponse = await aiClient.chat.completions.create({
-				model: 'grok-4-fast-non-reasoning',
-				messages: [
-					{
-						role: 'system',
-						content: pubconfig.prompt
-					},
-					{
-						role: 'user',
-						content: commandSen
-					}
-				]
-			});
+                .map((msg) => `${msg.role}: ${msg.content}`)
+                .join('\n')}`;
+            const chatResponse = await aiClient.chat.completions.create({
+                model: 'grok-4-fast-non-reasoning',
+                messages: [
+                    {
+                        role: 'system',
+                        content: pubconfig.prompt
+                    },
+                    {
+                        role: 'user',
+                        content: commandSen
+                    }
+                ]
+            });
 
-			const content = chatResponse.choices[0].message.content;
+            const content = chatResponse.choices[0].message.content;
 
-			const gifCommand = content.match(/\$\$gif of (.*?)\$\$/);
-			const truth = content.match(/\$\$answer:\s*(.*?)\s*\$\$/);
-			const truth2 = content.match(/\$\$answer:\s*(.*?)/);
+            const gifCommand = content.match(/\$\$gif of (.*?)\$\$/);
 
-			if (gifCommand) {
-				gif.execute(gifCommand, message);
-			}
-			if (truth) {
-				message.reply(truth?.[1]);
-			} else if (truth2) message.reply(truth2?.[1]);
-		} catch (err) {
-			notify.error('Error in jarvis.js', err, '-1x40113');
-		}
-	}
+            const truth = content.replace(/\$\$gif of (.*?)\$\$/, "");
+
+            if (gifCommand) {
+                gif.execute(gifCommand, message);
+            }
+            if (truth && truth != "") {
+                message.reply(truth?.[1]);
+            }
+        } catch (err) {
+            notify.error('Error in jarvis.js', err, '-1x40113');
+        }
+    }
 };
