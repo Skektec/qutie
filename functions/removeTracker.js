adad81150c9a543389a93214ce653b4a9a782cb5
Skekung <skekung.yt@gmail.com>
2025-12-17 16:38:17 +0200

    instagram link fix

--- functions/removeTracker.js
+++ functions/removeTracker.js
@@ -1,48 +1,48 @@
 module.exports = {
-	execute: async (message) => {
-		function delay(time) {
-			return new Promise((resolve) => setTimeout(resolve, time));
-		}
+    execute: async (message) => {
+        function delay(time) {
+            return new Promise((resolve) => setTimeout(resolve, time));
+        }
 
-		function suppressEmbed() {
-			message.suppressEmbeds(true);
-			delay(1000);
-			message.suppressEmbeds(true);
-		}
+        function suppressEmbed() {
+            message.suppressEmbeds(true);
+            delay(1000);
+            message.suppressEmbeds(true);
+        }
 
-		let cleaned = message.content;
+        let cleaned = message.content;
 
-		if (message.content.includes('http')) {
-			cleaned = cleaned.replace(
-				/(https?:\/\/(?:www\.)?reddit\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
-				'https://rxddit.com$2'
-			);
+        if (message.content.includes('http')) {
+            cleaned = cleaned.replace(
+                /(https?:\/\/(?:www\.)?reddit\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
+                'https://rxddit.com$2'
+            );
 
-			cleaned = cleaned.replace(
-				/(https?:\/\/(?:www\.)?instagram\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
-				'https://uuinstagram.com$2'
-			);
+            cleaned = cleaned.replace(
+                /(https?:\/\/(?:www\.)?instagram\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
+                'https://kkinstagram.com$2'
+            );
 
-			cleaned = cleaned.replace(
-				/(https?:\/\/(?:www\.)?x\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
-				'https://fixupx.com$2 ||'
-			);
+            cleaned = cleaned.replace(
+                /(https?:\/\/(?:www\.)?x\.com(\/[\w\/.-]+))(?:\?[^\s]*)?/gi,
+                'https://fixupx.com$2 ||'
+            );
 
-			// TODO: This can all be replaced with much simpler code
-			// instead of trying to match the entire link I should look for (in twitter for example) the status/XXXXXXXXX/ and then just attach to that depending on the preceding message.
+            // TODO: This can all be replaced with much simpler code
+            // instead of trying to match the entire link I should look for (in twitter for example) the status/XXXXXXXXX/ and then just attach to that depending on the preceding message.
 
-			if (cleaned === message.content) {
-				return;
-			} else {
-				try {
-					suppressEmbed();
-					return `-# ${cleaned}`;
-				} catch {
-					return `-# ${cleaned}`;
-				}
-			}
-		} else {
-			return;
-		}
-	}
+            if (cleaned === message.content) {
+
+            } else {
+                try {
+                    suppressEmbed();
+                    return `-# ${cleaned}`;
+                } catch {
+                    return `-# ${cleaned}`;
+                }
+            }
+        } else {
+
+        }
+    }
 };
