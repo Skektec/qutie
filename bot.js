e8aea60059e41a3c9b97a411a8f8a6e39c9e5699
Skekung <skekung.yt@gmail.com>
2026-01-05 15:22:31 +0200

    Added precise birthday checking

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const cron = require('node-cron');
-const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
-const { discordToken } = require('./data/config.json');
+const {Client, Collection, GatewayIntentBits, Partials} = require('discord.js');
+const {discordToken} = require('./data/config.json');
 const notify = require('./functions/notify');
-const { setClient } = require('./data/clientInstance');
+const {setClient} = require('./data/clientInstance');
 const database = require('./functions/database');
 const fetchNews = require('./functions/warThunder/fetchNews.js');
 const fetchDev = require('./functions/warThunder/fetchDev.js');
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildVoiceStates
-	],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates
+    ],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
 //Stores the client instance.
@@ -32,89 +32,95 @@ const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(
-				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-			);
-		}
-	}
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+            );
+        }
+    }
 }
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
 }
 
-cron.schedule('0 13 * * *', async () => {
-	try {
-		const today = new Date();
-		const currentDate = `${today.getDate()}-${today.toLocaleString('en-US', { month: 'long' })}`;
+// cron.schedule('0 13 * * *', async () => {
+try {
+    const today = new Date();
+    const currentDate = `${today.getDate()}-${today.toLocaleString('en-US', {month: 'long'})}`;
 
-		database.query(
-			`SELECT * FROM birthdays WHERE date LIKE '%${currentDate}%'`,
-			async (err, result) => {
-				if (err) {
-					notify.error('Error querying the database:', err, '1x36079');
-					return;
-				}
+    database.query(
+        `SELECT *
+         FROM birthdays
+         WHERE date LIKE '%${currentDate}%'`,
+        async (err, result) => {
+            if (err) {
+                notify.error('Error querying the database:', err, '1x36079');
+                return;
+            }
 
-				const rows = result.rows;
+            const rows = result.rows;
 
-				if (rows.length === 0) {
-					return;
-				}
+            if (rows.length === 0) {
+                return;
+            }
 
-				for (const row of rows) {
-					try {
-						const birthdayUser = row.id && row.id !== '0' ? `<@${row.id}>` : row.nick || 'unknown';
-						const channel = await client.channels.fetch(row.channel);
+            for (const row of rows) {
+                try {
 
-						if (!channel) {
-							console.log(`Channel ${row.channel} not found`);
-							continue;
-						}
+                    const dateLength = row.date.length;
+                    if (row.date.slice(0, dateLength - 5) !== currentDate) return;
 
-						channel.send(`🎉 Happy Birthday ${birthdayUser}! 🎉`);
-						return;
-					} catch (channelError) {
-						notify.error(
-							`Error fetching channel ${row.channel} or id ${row.id} or nick ${row.nick}:`,
-							channelError,
-							'4x36104'
-						);
-						return;
-					}
-				}
-			}
-		);
-	} catch (err) {
-		notify.error('Error displaying birthday:', err, '2x36112');
-	}
-});
+                    const birthdayUser = row.id && row.id !== '0' ? `<@${row.id}>` : row.nick || 'unknown';
+                    const channel = await client.channels.fetch(row.channel);
+
+                    if (!channel) {
+                        console.log(`Channel ${row.channel} not found`);
+                        continue;
+                    }
+
+                    channel.send(`🎉 Happy Birthday ${birthdayUser}! 🎉`);
+                    return;
+                } catch (channelError) {
+                    notify.error(
+                        `Error fetching channel ${row.channel} or id ${row.id} or nick ${row.nick}:`,
+                        channelError,
+                        '4x36104'
+                    );
+                    return;
+                }
+            }
+        }
+    );
+} catch (err) {
+    notify.error('Error displaying birthday:', err, '2x36112');
+}
+// });
 
 cron.schedule('10 * * * *', async () => {
-	fetchNews.findLinks();
-	fetchDev.findLinks();
+    fetchNews.findLinks();
+    fetchDev.findLinks();
 });
 cron.schedule('30 * * * *', async () => {
-	fetchNews.findLinks();
-	fetchDev.findLinks();
+    fetchNews.findLinks();
+    fetchDev.findLinks();
 });
 cron.schedule('50 * * * *', async () => {
-	fetchNews.findLinks();
-	fetchDev.findLinks();
+    fetchNews.findLinks();
+    fetchDev.findLinks();
 });
 
 client.login(discordToken);
