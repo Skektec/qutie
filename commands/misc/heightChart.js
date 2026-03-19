cd5193d20df675f63309e38c3b253afce6e37649
Skekung <skekung.yt@gmail.com>
2026-03-20 00:47:46 +0200

    fix

--- commands/misc/heightChart.js
+++ commands/misc/heightChart.js
@@ -8,8 +8,9 @@ module.exports = {
         const heightEmbed = new EmbedBuilder()
             .setColor(0x0099ff)
             .setTitle("Heights")
-            .setImage(`https://media.discordapp.net/attachments/1034191014451236894/1484315659486892113/HeightComparison-chart.png?ex=69bdc82e&is=69bc76ae&hm=91c589955385f3ea6ba5bacf86c084dfbd24f83a3838a15f971e5d9863454ef1&=&format=webp&quality=lossless&width=1656&height=668`);
+            .setImage(`https://media.discordapp.net/attachments/1034191014451236894/1484315659486892113/HeightComparison-chart.png?ex=69bdc82e&is=69bc76ae&hm=91c589955385f3ea6ba5bacf86c084dfbd24f83a3838a15f971e5d9863454ef1&=&format=webp&quality=lossless&width=1656&height=668`)
+            .setFooter({text: "Aww cuties"});
 
-        interaction.reply(heightEmbed)
+        interaction.reply({embeds: [heightEmbed]})
     },
 };
