1d02831762e66a62192bb6e1db73caa74d0a54bf
Skekung <skekung.yt@gmail.com>
2026-06-28 17:44:38 +0200

    fixes

--- functions/removeTracker.js
+++ functions/removeTracker.js
@@ -31,7 +31,7 @@ module.exports = {
             // TODO: This can all be replaced with much simpler code
             // instead of trying to match the entire link I should look for (in Twitter for example) the status/XXXXXXXXX/ and then just attach to that depending on the preceding message.
 
-            if (cleaned !== message.content) {
+            if (cleaned != message.content) {
                 try {
                     suppressEmbed();
                     return `-# ${cleaned}`;
