0dc39d62be821d4a3a0bbc4ee64f368f6c732e00
Skekung <skekung.yt@gmail.com>
2026-07-03 16:31:25 +0200

    removed stream ping

--- functions/warThunder/newsTypes/esport.js
+++ functions/warThunder/newsTypes/esport.js
@@ -62,21 +62,8 @@ module.exports = {
 
 			newChannels.forEach((element) => {
 				async function getChannel() {
-					let ping = null;
-
-					if (
-						$('.content__title')
-							.text()
-							.trim()
-							.match(/.*Winner.*/)
-					) {
-						ping = null;
-					} else {
-						ping = `<@&${element.role}>`;
-					}
-
 					const channel = await client.channels.fetch(element.channel);
-					channel.send({ content: ping, embeds: [esportEmbed] });
+					channel.send({ embeds: [esportEmbed] });
 				}
 				getChannel();
 			});
